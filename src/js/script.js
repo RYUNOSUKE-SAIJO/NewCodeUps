8659f91ef7d977ec900bf1f9124a58e64178210d
西城 龍乃介 <>
2023-11-24 19:33:33 +0900

    11/24 ゆうきさん修正完了まで

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+jQuery(function ($) {
+    // この中であればWordpressでも「$」が使用可能になる
     $(".js-hamburger,.js-drawer, .js-drawer a").click(function () {
         $(".js-header,.js-hamburger").toggleClass("is-active");
         $(".js-drawer").fadeToggle(600);
@@ -9,29 +10,25 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     // リサイズ処理 //
     $(window).resize(function () {
         if (window.matchMedia("(min-width: 768px)").matches) {
-            closeDrawer();
+        closeDrawer();
         }
 
         function closeDrawer() {
-            $(".js-drawer,.js-hamburger").fadeOut();
-            $(".js-header,.js-hamburger").removeClass("is-active");
+        $(".js-drawer,.js-hamburger").fadeOut();
+        $(".js-header,.js-hamburger").removeClass("is-active");
         }
     });
-
-
-
 
     // FV swiper //
     var swiper = new Swiper(".js-fv-swiper", {
         loop: true,
-        effect: 'fade',
+        effect: "fade",
         speed: 1000,
         // autoplay: {
         //     delay: 5000,
         //     disableOnInteraction: false,
         // },
     });
-
 
     // campaign swiper //
     var swiper = new Swiper(".js-campaign-swiper", {
@@ -45,64 +42,48 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         //     delay: 2000,
         //     disableOnInteraction: false,
         // },
-        breakpoints: { //ブレークポイントの設定 小さい順に設定する！！
-            768: {
-                spaceBetween: 40,
-                width: 333,
-            },
+        breakpoints: {
+        //ブレークポイントの設定 小さい順に設定する！！
+        768: {
+            spaceBetween: 40,
+            width: 333,
+        },
         },
         navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
         },
     });
 
-
     // 画像アニメーション //
-    var box = $('.information__left-img,.voice-card__img,.price__img,.tab-contents__panel-right'),
-    speed = 500;  
+    var box = $(
+        ".information__left-img,.voice-card__img,.price__img,.tab-contents__panel-right"
+        ),
+        speed = 500;
 
-//.colorboxの付いた全ての要素に対して下記の処理を行う
-    box.each(function(){
-        $(this).append('<div class="color"></div>')
-        var color = $(this).find($('.color')),
-        image = $(this).find('img');
+    //.colorboxの付いた全ての要素に対して下記の処理を行う
+    box.each(function () {
+        $(this).append('<div class="color"></div>');
+        var color = $(this).find($(".color")),
+        image = $(this).find("img");
         var counter = 0;
 
-        image.css('opacity','0');
-        color.css('width','0%');
+        image.css("opacity", "0");
+        color.css("width", "0%");
         //inviewを使って背景色が画面に現れたら処理をする
-        color.on('inview', function(){
-            if(counter == 0){
-            $(this).delay(100).animate({'width':'100%'},speed,function(){
-                image.css('opacity','1');
-                $(this).css({'left':'0' , 'right':'auto'});
-                $(this).animate({'width':'0%'},speed);
-            })
-                counter = 1;
-            }
-        });
-
-    });
-
-    // スクロールしたら「トップに戻る」ボタンが表示される
-    const toTopButton = $(".js-to-top");
-    const scrollHeight = 100;
-    toTopButton.hide();
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-        toTopButton.fadeIn();
-        } else {
-        toTopButton.fadeOut();
+        color.on("inview", function () {
+        if (counter == 0) {
+            $(this)
+            .delay(100)
+            .animate({ width: "100%" }, speed, function () {
+                image.css("opacity", "1");
+                $(this).css({ left: "0", right: "auto" });
+                $(this).animate({ width: "0%" }, speed);
+            });
+            counter = 1;
         }
+        });
     });
-
-    // 「トップに戻る」ボタンをクリックしたらスクロールで戻る
-    toTopButton.click(function () {
-        $("body,html").animate({ scrollTop: 0 }, 800);
-        return false;
-    });
-
 
     // modal window //
     $(".gallery__item > img").click(function () {
@@ -115,35 +96,28 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     $(".js-gallery__modal-block").click(function (e) {
         // モーダルの外側をクリックした場合にのみモーダルを閉じる
         if (e.target === this) {
-            $(this).fadeOut(); // フェードアウトさせる
+        $(this).fadeOut(); // フェードアウトさせる
         }
     });
 
-
     // タブ切替 //
-    $('.js-tab-contents__tab').on('click', function() {
-        $('.js-tab-contents__tab,.js-tab-contents__panel').removeClass('is-active');
-    
-        $(this).addClass('is-active');
+    $(".js-tab-contents__tab").on("click", function () {
+        $(".js-tab-contents__tab,.js-tab-contents__panel").removeClass("is-active");
 
-        var index = $('.js-tab-contents__tab').index(this);
-        $('.js-tab-contents__panel').eq(index).addClass('is-active');
+        $(this).addClass("is-active");
+
+        var index = $(".js-tab-contents__tab").index(this);
+        $(".js-tab-contents__panel").eq(index).addClass("is-active");
     });
-
 
     // アコーディオン //
 
     $(function () {
-      //アイテムとコンテンツを開きっぱなしにする記述//
-        $(".js-accordion__item .js-accordion__content").css(
-        "display",
-        "block"
-        );
-      //開きっぱなしのアイテムの＋ボタンの記述//
-        $(".js-accordion__item .js-accordion__title").addClass(
-        "is-open"
-        ) ;
-      //クリックしたら..//
+        //アイテムとコンテンツを開きっぱなしにする記述//
+        $(".js-accordion__item .js-accordion__content").css("display", "block");
+        //開きっぱなしのアイテムの＋ボタンの記述//
+        $(".js-accordion__item .js-accordion__title").addClass("is-open");
+        //クリックしたら..//
         $(".js-accordion__title").on("click", function () {
         //"is-open"が付与されて＋ボタンがーボタンに//
         $(this).toggleClass("is-open");
@@ -152,18 +126,64 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         });
     });
 
+    // トップに戻るボタン //
+
+    // スクロールしたら「トップに戻る」ボタンが表示される
+    // const toTopButton = $(".js-to-top");
+    // const scrollHeight = 50;
+    // toTopButton.hide();
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 50) {
+    //     toTopButton.fadeIn();
+    //     } else {
+    //     toTopButton.fadeOut();
+    //     }
+    // });
+
+    // トップへ戻るボタン
+    $(window).on("scroll", function () {
+        let scrollHeight = $(document).height();
+        let scrollPosition = $(window).height() + $(window).scrollTop();
+        let footHeight = $("footer").innerHeight();
+        if (scrollHeight - scrollPosition <= footHeight) {
+        $(".js-to-top").css({
+            position: "absolute",
+            bottom: footHeight + 19,
+        });
+        } else {
+        $(".js-to-top").css({
+            position: "fixed",
+            bottom: "16px",
+        });
+        9;
+        }
+    });
+
+    let topBtn = $("#page-top");
+    topBtn.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+        topBtn.fadeIn();
+        } else {
+        topBtn.fadeOut();
+        }
+    });
+    topBtn.click(function () {
+        $("body,html").animate(
+        {
+            scrollTop: 0,
+        },
+        500,
+        "swing"
+        );
+        return false;
+    });
 
 
 
-
-
-
-
-
-
-
-
-
+    
 });
+
+
 
 
